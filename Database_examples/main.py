d6e43b2d4dddd19853b2aac63e0cd461f3180080
chanab0910 <chanab0910@highgateschool.org.uk>
2023-01-12 09:18:39 +0000

    example

---
+++ Database_examples/main.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+# Create a connection to a database - if one does not exist, a new one will be created 
+conn = sqlite3.connect("student.sqlite")
+
+# A cursor is a pointer to a place in the database which allows access 
+# to a table row-by-row 
+cursor = conn.cursor()
+
+# SQL commands can be written as text and then 'run' using an execute command 
+# Example - creating a table 
+create_students_table = """ 
+CREATE TABLE IF NOT EXISTS students ( 
+  id INTEGER PRIMARY KEY AUTOINCREMENT, 
+  firstname TEXT NOT NULL, 
+  lastname TEXT NOT NULL, 
+  age INTEGER, 
+  gender TEXT 
+); 
+"""
+cursor.execute(create_students_table)
+
+conn.close() 
