0304df456ca38d1017fbb37e8d47a74170fbd31b
Chanab0910 <119660917+Chanab0910@users.noreply.github.com>
2023-01-12 16:02:55 +0000

    Update Read_from_database.py

--- Database_examples/Read_from_database.py
+++ Database_examples/Read_from_database.py
@@ -13,3 +13,25 @@ cursor.execute(select_students)
 first_student = cursor.fetchone()
 more_students = cursor.fetchmany(10)
 other_students = cursor.fetchall()
+
+student_j = """
+SELECT substr(firstname, 0, 1)
+FROM students
+
+"""
+student_j = cursor.fetchone()
+print(student_j)
+
+average_query = """ 
+SELECT avg(age) 
+FROM students 
+WHERE gender = ? 
+"""
+average_age = cursor.execute(average_query, ('female',)).fetchone()[0]
+
+group_by_query = """ 
+SELECT gender, sum(age)
+FROM students 
+GROUP BY gender 
+"""
+age_by_gender = cursor.execute(group_by_query).fetchall()
