f7aae39a394e9c646e51378cf90dca00e88af3d7
Chanab0910 <119660917+Chanab0910@users.noreply.github.com>
2023-02-25 12:06:55 +0000

    Update models.py

--- Database_examples/models.py
+++ Database_examples/models.py
@@ -1,21 +1,29 @@
 from sqlalchemy import Column, Integer, String, Date, Boolean, Table, UniqueConstraint, ForeignKey
-from sqlalchemy.orm import declarative_base, backref
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
-book_author = Table("book_author", Base.metadata, Column('book_id', ForeignKey('book.book_id')),
-                    Column('author_id', ForeignKey("author.author_id")), UniqueConstraint('book_id', 'author_id'))
+book_author = Table("book_author",
+                    Base.metadata,
+                    Column('book_id', ForeignKey('book.book_id')),
+                    Column('author_id', ForeignKey("author.author_id")),
+                    UniqueConstraint('book_id', 'author_id')
+                    )
 
-book_loan = Table("book_loan", Base.metadata, Column('book_id', ForeignKey('book.book_id')),
-                  Column('person_id', ForeignKey("person.person_id")), UniqueConstraint('book_id', 'person_id'))
+book_loan = Table("book_loan",
+                  Base.metadata,
+                  Column('book_id', ForeignKey('book.book_id')),
+                  Column('person_id', ForeignKey("person.person_id")),
+                  UniqueConstraint('book_id', 'person_id')
+                  )
 
 
 class Book(Base):
-    __tablename__ = "Book"
+    __tablename__ = "book"
     book_id = Column(Integer, primary_key=True, autoincrement=True)
     title = Column(String)
-    isbn = Column(String)
+    isbn = Column(String, unique=True)
     num_pages = Column(Integer)
     publication_date = Column(Date)
     publisher_id = Column(Integer, ForeignKey("publisher.publisher_id"))
@@ -30,17 +38,23 @@ class Book(Base):
                              order_by='(Person.name)',
                              back_populates='on_loan')
 
+    publisher = relationship("Publisher", back_populates="books")
+
     def __repr__(self):
-        return f"<Book({self.name})>"
+        return f"Book(title='{self.title}',"  \
+               f"isbn={self.isbn}), " \
+               f"num_pages={self.num_pages}, " \
+               f"publication_date={self.publication_date})"
 
 
 class Publisher(Base):
     __tablename__ = "publisher"
     publisher_id = Column(Integer, primary_key=True, autoincrement=True)
     publisher_name = Column(String)
+    books = relationship("Book", back_populates="publisher")
 
     def __repr__(self):
-        return f"<Publisher({self.name})>"
+        return f"Publisher(publisher_name='{self.publisher_name}')"
 
 
 class Author(Base):
@@ -54,7 +68,7 @@ class Author(Base):
                          back_populates="authors")
 
     def __repr__(self):
-        return f"<Author({self.name})>"
+        return f"Author(author_name='{self.author_name}'))"
 
 
 class Person(Base):
@@ -69,4 +83,4 @@ class Person(Base):
                            back_populates="borrowers")
 
     def __repr__(self):
-        return f"<Person({self.name})>"
+        return f"Person(name='{self.name}')"
